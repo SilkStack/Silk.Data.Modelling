7f57945071cf1342cf20a93f83a63d33e3871004
John Carruthers <johnc@frag-labs.com>
2017-11-06 13:16:46 -0600

    Return null when parent objects in GetValue graph are null.

--- Silk.Data.Modelling/ObjectContainer.cs
+++ Silk.Data.Modelling/ObjectContainer.cs
@@ -32,11 +32,19 @@ namespace Silk.Data.Modelling
 		public object GetValue(string[] fieldPath)
 		{
 			//  todo: replace reflection with cached expressions
-			//  todo: how to support field paths with more than 1 element?
-			var property = DataType.GetProperty(fieldPath[0]);
-			if (property == null)
-				throw new InvalidOperationException("Field cannot be got on view.");
-			return property.GetValue(Instance);
+			object ret = null;
+			var dataType = DataType;
+			foreach (var pathComponent in fieldPath)
+			{
+				var property = dataType.GetProperty(pathComponent);
+				if (property == null)
+					throw new InvalidOperationException("Field cannot be got on view.");
+				ret = property.GetValue(ret ?? Instance);
+				if (ret == null)
+					break;
+				dataType = ret.GetType();
+			}
+			return ret;
 		}
 	}
 }
