efded76b3d1f5b8041e4596ab5d32f832de8fe9d
John Carruthers <johnc@frag-labs.com>
2017-11-11 14:33:44 -0600

    Implement memory containers and remove deprecated (and useless) MemoryContainer.

--- Silk.Data.Modelling/MemoryContainer.cs
+++ Silk.Data.Modelling/MemoryContainer.cs
@@ -2,36 +2,6 @@
 
 namespace Silk.Data.Modelling
 {
-	/// <summary>
-	/// Stores a graph in memory.
-	/// </summary>
-	/// <remarks>Really designed as an example container.</remarks>
-	public class MemoryContainer : IContainer
-	{
-		public TypedModel Model { get; }
-
-		public IView View { get; }
-
-		public Dictionary<string, object> Data { get; } = new Dictionary<string, object>();
-
-		public MemoryContainer(TypedModel model, IView view)
-		{
-			Model = model;
-			View = view;
-		}
-
-		public void SetValue(string[] fieldPath, object value)
-		{
-			Data[string.Join(".", fieldPath)] = value;
-		}
-
-		public object GetValue(string[] fieldPath)
-		{
-			Data.TryGetValue(string.Join(".", fieldPath), out var value);
-			return value;
-		}
-	}
-
 	public class MemoryModelReadWriter : ModelReadWriter
 	{
 		public MemoryModelReadWriter(Model model, Dictionary<string,object> store = null)
@@ -47,12 +17,15 @@ namespace Silk.Data.Modelling
 
 		public override T ReadFromPath<T>(string[] path)
 		{
-			throw new System.NotImplementedException();
+			Store.TryGetValue(string.Join(".", path), out var value);
+			if (value == null)
+				return default(T);
+			return (T)value;
 		}
 
 		public override void WriteToPath<T>(string[] path, T value)
 		{
-			throw new System.NotImplementedException();
+			Store[string.Join(".", path)] = value;
 		}
 	}
 
@@ -71,12 +44,15 @@ namespace Silk.Data.Modelling
 
 		public override T ReadFromPath<T>(string[] path)
 		{
-			throw new System.NotImplementedException();
+			Store.TryGetValue(string.Join(".", path), out var value);
+			if (value == null)
+				return default(T);
+			return (T)value;
 		}
 
 		public override void WriteToPath<T>(string[] path, T value)
 		{
-			throw new System.NotImplementedException();
+			Store[string.Join(".", path)] = value;
 		}
 	}
 }
