e532cc601ac8497e4d6e8766d68577b174869121
John Carruthers <johnc@frag-labs.com>
2018-01-27 13:16:41 -0600

    Add IMapper API.

---
+++ Silk.Data.Modelling/DefaultConventionsMapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Silk.Data.Modelling
+{
+	public class DefaultConventionsMapper<T> : IMapper<T>
+		 where T : new()
+	{
+		public Type ModelType { get; }
+		public TypedModel<T> Model { get; }
+
+		private readonly Dictionary<Type, TypedDefaultView<T>> _viewCache = new Dictionary<Type, TypedDefaultView<T>>();
+
+		public DefaultConventionsMapper()
+		{
+			ModelType = typeof(T);
+			Model = TypeModeller.GetModelOf<T>();
+		}
+
+		private TypedDefaultView<T,TView> GetView<TView>()
+		{
+			var viewType = typeof(TView);
+			TypedDefaultView<T> view;
+			if (_viewCache.TryGetValue(viewType, out view))
+				return view as TypedDefaultView<T, TView>;
+
+			lock (_viewCache)
+			{
+				if (_viewCache.TryGetValue(viewType, out view))
+					return view as TypedDefaultView<T, TView>;
+
+				view = Model.CreateTypedView<T, TView>();
+				_viewCache.Add(viewType, view);
+				return view as TypedDefaultView<T, TView>;
+			}
+		}
+
+		public Task<T> MapAsync<TView>(TView viewInstance) where TView : new()
+		{
+			return GetView<TView>()
+				.MapToModelAsync(viewInstance);
+		}
+
+		public Task<TView> MapAsync<TView>(T modelInstance) where TView : new()
+		{
+			return GetView<TView>()
+				.MapToViewAsync(modelInstance);
+		}
+
+		public Task MapAsync<TView>(T from, TView onto)
+		{
+			return GetView<TView>()
+				.MapToViewAsync(from, onto);
+		}
+
+		public Task MapAsync<TView>(TView from, T onto)
+		{
+			return GetView<TView>()
+				.MapToModelAsync(onto, from);
+		}
+	}
+}
---
+++ Silk.Data.Modelling/IMapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Silk.Data.Modelling
+{
+	public interface IMapper<T> where T : new()
+	{
+		Type ModelType { get; }
+		TypedModel<T> Model { get; }
+
+		Task<T> MapAsync<TView>(TView viewInstance)
+			where TView : new();
+		Task<TView> MapAsync<TView>(T modelInstance)
+			where TView : new();
+		Task MapAsync<TView>(T from, TView onto);
+		Task MapAsync<TView>(TView from, T onto);
+	}
+}
