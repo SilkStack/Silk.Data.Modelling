2677487c01a4d670374c164d78154fdded63ccbd
John Carruthers <johnc@frag-labs.com>
2018-02-26 00:06:23 -0600

    Added InflateSameTypes convention.

---
+++ Silk.Data.Modelling.Tests/InflateSameTypesTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Silk.Data.Modelling.Mapping;
+using System.Linq;
+
+namespace Silk.Data.Modelling.Tests
+{
+	[TestClass]
+	public class InflateSameTypesTests
+	{
+		[TestMethod]
+		public void InflateSameTypesBindsReadWriteProperties()
+		{
+			var mapping = CreateMapping<FlatData, SourceRoot>();
+			Assert.AreEqual(1, mapping.Bindings.Length);
+
+			var flattenBinding = mapping.Bindings[0];
+			Assert.IsInstanceOfType(flattenBinding, typeof(CopyBinding<int>));
+			Assert.IsTrue(flattenBinding.ToPath.SequenceEqual(new[] { "Data", "Property" }));
+			Assert.IsTrue(flattenBinding.FromPath.SequenceEqual(new[] { "DataProperty" }));
+		}
+
+		private Mapping.Mapping CreateMapping<T>()
+		{
+			return CreateMapping<T, T>();
+		}
+
+		private Mapping.Mapping CreateMapping<TFrom, TTo>()
+		{
+			var fromPocoModel = TypeModel.GetModelOf<TFrom>();
+			var toPocoModel = TypeModel.GetModelOf<TTo>();
+
+			var builder = new MappingBuilder(fromPocoModel, toPocoModel);
+			builder.AddConvention(InflateSameTypes.Instance);
+			return builder.BuildMapping();
+		}
+
+		private class SourceRoot
+		{
+			public SourceData Data { get; set; }
+		}
+
+		private class SourceData
+		{
+			public int Property { get; set; }
+		}
+
+		public class FlatData
+		{
+			public int DataProperty { get; set; }
+		}
+	}
+}
---
+++ Silk.Data.Modelling/Mapping/InflateSameTypes.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+
+namespace Silk.Data.Modelling.Mapping
+{
+	public class InflateSameTypes : IMappingConvention
+	{
+		public static InflateSameTypes Instance { get; } = new InflateSameTypes();
+
+		public void CreateBindings(SourceModel fromModel, TargetModel toModel, MappingBuilder builder)
+		{
+			foreach (var fromField in fromModel.Fields.Where(q => q.CanRead))
+			{
+				var potentialTargetPaths = ConventionUtilities.GetPaths(fromField.FieldName).ToArray();
+
+				ITargetField toField = null;
+
+				foreach (var targetPath in potentialTargetPaths)
+				{
+					var testField = toModel.GetField(targetPath);
+					if (testField == null || !testField.CanWrite || testField.FieldType != fromField.FieldType ||
+						builder.IsBound(testField))
+						continue;
+					toField = testField;
+					break;
+				}
+				if (toField == null)
+					continue;
+
+				builder
+					.Bind(toField)
+					.From(fromField)
+					.Using<CopyBinding>();
+			}
+
+			foreach (var toField in toModel.Fields.Where(q => q.CanWrite && !builder.IsBound(q)))
+			{
+				var potentialSourcePaths = ConventionUtilities.GetPaths(toField.FieldName).ToArray();
+
+				ISourceField fromField = null;
+
+				foreach (var sourcePath in potentialSourcePaths)
+				{
+					var testField = fromModel.GetField(sourcePath);
+					if (testField == null || !testField.CanRead || testField.FieldType != toField.FieldType)
+						continue;
+					fromField = testField;
+					break;
+				}
+				if (fromField == null)
+					continue;
+
+				builder
+					.Bind(toField)
+					.From(fromField)
+					.Using<CopyBinding>();
+			}
+		}
+	}
+}
