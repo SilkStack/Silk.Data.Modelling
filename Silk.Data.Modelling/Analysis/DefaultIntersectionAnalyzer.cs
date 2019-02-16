a148d74cee0b3e1f5f56397c5db15c317d25fb24
John Carruthers <johnc@frag-labs.com>
2019-02-16 02:05:39 -0600

    Add DefaultIntersectionAnalyzer.

---
+++ Silk.Data.Modelling/Analysis/DefaultIntersectionAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using Silk.Data.Modelling.Analysis.CandidateSources;
+using Silk.Data.Modelling.Analysis.Rules;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Silk.Data.Modelling.Analysis
+{
+	public class DefaultIntersectionAnalyzer<TLeftModel, TLeftField, TRightModel, TRightField> :
+		IntersectAnalyzerBase<TLeftModel, TLeftField, TRightModel, TRightField>
+		where TLeftModel : IModel<TLeftField>
+		where TRightModel : IModel<TRightField>
+		where TLeftField : class, IField
+		where TRightField : class, IField
+	{
+		public DefaultIntersectionAnalyzer(
+			IEnumerable<IIntersectCandidateSource<TLeftModel, TLeftField, TRightModel, TRightField>> intersectCandidateSources = null,
+			IEnumerable<IIntersectionRule<TLeftModel, TLeftField, TRightModel, TRightField>> intersectionRules = null
+			)
+		{
+			AddCandidateSources(intersectCandidateSources);
+			AddIntersectionRules(intersectionRules);
+		}
+
+		protected override IIntersection<TLeftModel, TLeftField, TRightModel, TRightField> CreateIntersection(
+			IntersectAnalysis analysis
+			)
+			=> new Intersection(analysis.LeftModel, analysis.RightModel, analysis.IntersectedFields.ToArray());
+
+		protected virtual IEnumerable<IIntersectionRule<TLeftModel, TLeftField, TRightModel, TRightField>> GetDefaultRules()
+		{
+			yield return new SameDataTypeRule<TLeftModel, TLeftField, TRightModel, TRightField>();
+			yield return new BothNumericTypesRule<TLeftModel, TLeftField, TRightModel, TRightField>();
+			yield return new ConvertableWithToStringRule<TLeftModel, TLeftField, TRightModel, TRightField>();
+			yield return new ExplicitCastRule<TLeftModel, TLeftField, TRightModel, TRightField>();
+			yield return new ConvertableWithTryParse<TLeftModel, TLeftField, TRightModel, TRightField>();
+		}
+
+		protected virtual IEnumerable<IIntersectCandidateSource<TLeftModel, TLeftField, TRightModel, TRightField>> GetDefaultCandidateSources()
+		{
+			yield return new ExactPathMatchCandidateSource<TLeftModel, TLeftField, TRightModel, TRightField>();
+			yield return new FlattenedNameMatchCandidateSource<TLeftModel, TLeftField, TRightModel, TRightField>();
+		}
+
+		private void AddCandidateSources(IEnumerable<IIntersectCandidateSource<TLeftModel, TLeftField, TRightModel, TRightField>> intersectCandidateSources)
+		{
+			foreach (var item in intersectCandidateSources ?? GetDefaultCandidateSources())
+				CandidateSources.Add(item);
+		}
+
+		private void AddIntersectionRules(IEnumerable<IIntersectionRule<TLeftModel, TLeftField, TRightModel, TRightField>> intersectionRules)
+		{
+			foreach (var item in intersectionRules ?? GetDefaultRules())
+				IntersectionRules.Add(item);
+		}
+
+		public class Intersection :
+			IntersectionBase<TLeftModel, TLeftField, TRightModel, TRightField>
+		{
+			public Intersection(TLeftModel leftModel, TRightModel rightModel,
+				IntersectedFields<TLeftModel, TLeftField, TRightModel, TRightField>[] intersectedFields) :
+				base(leftModel, rightModel, intersectedFields)
+			{
+			}
+		}
+	}
+}
--- Silk.Data.Modelling/Analysis/TypeToTypeIntersectionAnalyzer.cs
+++ Silk.Data.Modelling/Analysis/TypeToTypeIntersectionAnalyzer.cs
@@ -1,59 +1,19 @@
 ﻿using Silk.Data.Modelling.Analysis.CandidateSources;
 using Silk.Data.Modelling.Analysis.Rules;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Silk.Data.Modelling.Analysis
 {
 	/// <summary>
 	/// Analyzes two TypeModels to generate a TypeToTypeIntersection.
 	/// </summary>
-	public class TypeToTypeIntersectionAnalyzer : IntersectAnalyzerBase<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>
+	public class TypeToTypeIntersectionAnalyzer : DefaultIntersectionAnalyzer<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>
 	{
 		public TypeToTypeIntersectionAnalyzer(
 			IEnumerable<IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> intersectCandidateSources = null,
 			IEnumerable<IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> intersectionRules = null
-			)
+			) : base(intersectCandidateSources, intersectionRules)
 		{
-			AddCandidateSources(intersectCandidateSources);
-			AddIntersectionRules(intersectionRules);
-		}
-
-		private void AddCandidateSources(IEnumerable<IIntersectCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> intersectCandidateSources)
-		{
-			if (intersectCandidateSources == null)
-			{
-				CandidateSources.Add(new ExactPathMatchCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-				CandidateSources.Add(new FlattenedNameMatchCandidateSource<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-
-				return;
-			}
-
-			foreach (var item in intersectCandidateSources)
-				CandidateSources.Add(item);
-		}
-
-		private void AddIntersectionRules(IEnumerable<IIntersectionRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>> intersectionRules)
-		{
-			if (intersectionRules == null)
-			{
-				IntersectionRules.Add(new SameDataTypeRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-				IntersectionRules.Add(new BothNumericTypesRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-				IntersectionRules.Add(new ConvertableWithToStringRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-				IntersectionRules.Add(new ExplicitCastRule<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-				IntersectionRules.Add(new ConvertableWithTryParse<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField>());
-
-				return;
-			}
-
-			foreach (var item in intersectionRules)
-				IntersectionRules.Add(item);
-		}
-
-		protected override IIntersection<TypeModel, PropertyInfoField, TypeModel, PropertyInfoField> CreateIntersection(
-			IntersectAnalysis analysis)
-		{
-			return new TypeToTypeIntersection(analysis.LeftModel, analysis.RightModel, analysis.IntersectedFields.ToArray());
 		}
 	}
 }
